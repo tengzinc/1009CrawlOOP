2beab6cc5fc24d8b6e95ec38210be86e98099d60
Bandung-2000 <78348397+Bandung-2000@users.noreply.github.com>
2021-03-20 13:53:38 +0800

    rowcount error

--- socialMediaObjectCreator.py
+++ socialMediaObjectCreator.py
@@ -161,12 +161,7 @@ class socialMedia:
             self.__overallSentiment+=self.__analyzeSentiment(x[3])
             rowCount+=1
         if rowCount!=0:
-<<<<<<< HEAD
-            self.__overallSentiment=self.__overallSentiment/rowCount
-        
-=======
             self.__overallSentiment=self.__overallSentiment/rowCount    
->>>>>>> 7b7f613bd8be56c2d56d6dd569edab469e88faf7
 
     def __categorizeSentiment(self):
         """
